6af05cca9fa579b8744658b827c15bfca0695469
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-08 06:29:11 +0530

    cnss2: qmi: Load hw specific firmware for k81/k81a
    Change-Id: I743df8fe78695b4fde8502ed862349a2be41b4e3
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/net/wireless/cnss2/qmi.c
+++ drivers/net/wireless/cnss2/qmi.c
@@ -37,7 +37,11 @@
 #define ELF_BDF_FILE_NAME_K11A_GLOBAL	 "bd_k11agl.elf"
 #define ELF_BDF_FILE_NAME_K11A_INDIA	 "bd_k11ain.elf"
 
+#define ELF_BDF_FILE_NAME_K81            "bd_k81.elf"
+#define ELF_BDF_FILE_NAME_K81A           "bd_k81a.elf"
+
 #define ELF_BDF_FILE_NAME_GF		"bdwlang.elf"
+
 #define ELF_BDF_FILE_NAME_PREFIX	"bdwlan.e"
 #define ELF_BDF_FILE_NAME_GF_PREFIX	"bdwlang.e"
 #define BIN_BDF_FILE_NAME		"bdwlan.bin"
@@ -571,6 +575,10 @@ static int cnss_get_bdf_file_name(struct cnss_plat_data *plat_priv,
 				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K11A_INDIA);
 				else
 				    snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K11A);
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_ENUMA) {
+				snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K81);
+			} else if (hw_platform_ver == HARDWARE_PLATFORM_ELISH) {
+				snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_K81A);
 			} else {
 				if (hw_country_ver == (uint32_t)CountryGlobal)
 					snprintf(filename_tmp, filename_len, ELF_BDF_FILE_NAME_GLOBAL);
