UtsavBalar1231/kernel_xiaomi_sm8250

debug: Add support to log wakeup reason triggered by irqs
Change-Id: I4ee851f513b07f77a9552952fef485583caef8ae

drivers/irqchip/irq-gic-v3.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013-2017 ARM Limited, All Rights Reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Author: Marc Zyngier <marc.zyngier@arm.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -34,6 +35,9 @@
 #include <linux/irqchip/arm-gic-common.h>
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/irqchip/irq-partition-percpu.h>
+#include <linux/power_debug.h>
+#include <linux/wakeup_reason.h>
+#include <linux/syscore_ops.h>
 
 #include <asm/cputype.h>
 #include <asm/exception.h>
@@ -371,7 +375,8 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
-		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		//pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		pr_warn("%s: %d triggered %s (hwirq %d)\n", __func__, irq, name, i);
 	}
 }
 
@@ -398,6 +403,43 @@ static int __init gic_init_sys(void)
 arch_initcall(gic_init_sys);
 
 #endif
+
+static bool gic_check_wakeup_event(void *data)
+{
+	unsigned int i;
+	u32 enabled;
+	bool ret=false;
+	u32 pending[32];
+	void __iomem *base = gic_data.dist_base;
+
+	for (i = 0; i * 32 < gic_data.irq_nr; i++) {
+		enabled = readl_relaxed(base + GICD_ICENABLER + i * 4);
+		pending[i] = readl_relaxed(base + GICD_ISPENDR + i * 4);
+		pending[i] &= enabled;
+	}
+
+	for (i = find_first_bit((unsigned long *)pending, gic_data.irq_nr);
+	     i < gic_data.irq_nr;
+	     i = find_next_bit((unsigned long *)pending, gic_data.irq_nr, i+1)) {
+		unsigned int irq = irq_find_mapping(gic_data.domain, i);
+		struct irq_desc *desc = irq_to_desc(irq);
+		const char *name = "null";
+
+		if (desc == NULL)
+			name = "stray irq";
+		else if (desc->action && desc->action->name)
+			name = desc->action->name;
+		ret = true;
+		log_wakeup_reason(irq);
+	}
+
+	return ret;
+}
+
+static struct wakeup_device gic_wakeup_device = {
+	.name = "gic-v3",
+	.check_wakeup_event = gic_check_wakeup_event,
+};
 
 static u64 gic_mpidr_to_affinity(unsigned long mpidr)
 {
@@ -1393,6 +1435,7 @@ static int __init gicv3_of_init(struct device_node *node, struct device_node *pa
 		goto out_unmap_rdist;
 
 	gic_populate_ppi_partitions(node);
+	pm_register_wakeup_device(&gic_wakeup_device);
 
 	if (static_branch_likely(&supports_deactivate_key))
 		gic_of_setup_kvm_info(node);

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -36,6 +36,9 @@
 #include <linux/bitmap.h>
 
 #include <linux/pinctrl/pinctrl_mi.h>
+#include <linux/power_debug.h>
+#include <linux/wakeup_reason.h>
+#include <linux/syscore_ops.h>
 
 #include "../core.h"
 #include "../pinconf.h"
@@ -1585,6 +1588,44 @@ int msm_gpio_mpm_wake_set(unsigned int gpio, bool enable)
 }
 EXPORT_SYMBOL(msm_gpio_mpm_wake_set);
 
+static bool msm_pinctrl_check_wakeup_event(void *data)
+{
+	int i, irq;
+	bool ret = false;
+	u32 val;
+	unsigned long flags;
+	struct irq_desc *desc;
+	const struct msm_pingroup *g;
+	const char *name = "null";
+	struct msm_pinctrl *pctrl= msm_pinctrl_data;
+
+	raw_spin_lock_irqsave(&pctrl->lock, flags);
+	for_each_set_bit(i, pctrl->enabled_irqs, pctrl->chip.ngpio) {
+		g = &pctrl->soc->groups[i];
+		val = readl_relaxed(pctrl->regs + g->intr_status_reg);
+		if (val & BIT(g->intr_status_bit)){
+			irq = irq_find_mapping(pctrl->chip.irq.domain, i);
+			log_wakeup_reason(irq);
+			desc = irq_to_desc(irq);
+			if (desc == NULL)
+				name = "stray_irq";
+			else if (desc->action && desc->action->name)
+				name = desc->action->name;
+			ret = true;
+			pr_warn("%s:%d triggered %s\n", __func__, irq, name);
+		}
+	}
+
+	raw_spin_unlock_irqrestore(&pctrl->lock, flags);
+
+	return ret;
+}
+
+static struct wakeup_device msm_pinctrl_wakeup_device = {
+	.name = "pinctrl-msm",
+	.check_wakeup_event = msm_pinctrl_check_wakeup_event,
+};
+
 int msm_pinctrl_probe(struct platform_device *pdev,
 		      const struct msm_pinctrl_soc_data *soc_data)
 {
@@ -1650,6 +1691,7 @@ int msm_pinctrl_probe(struct platform_device *pdev,
 	platform_set_drvdata(pdev, pctrl);
 
 	register_syscore_ops(&msm_pinctrl_pm_ops);
+	pm_register_wakeup_device(&msm_pinctrl_wakeup_device);
 	dev_dbg(&pdev->dev, "Probed Qualcomm pinctrl driver\n");
 
 	return 0;

rdtm443/macka_infinityx_sm8250_kali_boeffla

Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/irqchip/irq-gic-v3.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013-2017 ARM Limited, All Rights Reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Author: Marc Zyngier <marc.zyngier@arm.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -35,6 +36,9 @@
 #include <linux/irqchip/arm-gic-common.h>
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/irqchip/irq-partition-percpu.h>
+#include <linux/power_debug.h>
+#include <linux/wakeup_reason.h>
+#include <linux/syscore_ops.h>
 
 #include <asm/cputype.h>
 #include <asm/exception.h>
@@ -395,7 +399,8 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
-		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		//pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+		pr_warn("%s: %d triggered %s (hwirq %d)\n", __func__, irq, name, i);
 	}
 }
 
@@ -422,6 +427,43 @@ static int __init gic_init_sys(void)
 arch_initcall(gic_init_sys);
 
 #endif
+
+static bool gic_check_wakeup_event(void *data)
+{
+	unsigned int i;
+	u32 enabled;
+	bool ret=false;
+	u32 pending[32];
+	void __iomem *base = gic_data.dist_base;
+
+	for (i = 0; i * 32 < gic_data.irq_nr; i++) {
+		enabled = readl_relaxed(base + GICD_ICENABLER + i * 4);
+		pending[i] = readl_relaxed(base + GICD_ISPENDR + i * 4);
+		pending[i] &= enabled;
+	}
+
+	for (i = find_first_bit((unsigned long *)pending, gic_data.irq_nr);
+	     i < gic_data.irq_nr;
+	     i = find_next_bit((unsigned long *)pending, gic_data.irq_nr, i+1)) {
+		unsigned int irq = irq_find_mapping(gic_data.domain, i);
+		struct irq_desc *desc = irq_to_desc(irq);
+		const char *name = "null";
+
+		if (desc == NULL)
+			name = "stray irq";
+		else if (desc->action && desc->action->name)
+			name = desc->action->name;
+		ret = true;
+		log_irq_wakeup_reason(irq);
+	}
+
+	return ret;
+}
+
+static struct wakeup_device gic_wakeup_device = {
+	.name = "gic-v3",
+	.check_wakeup_event = gic_check_wakeup_event,
+};
 
 static u64 gic_mpidr_to_affinity(unsigned long mpidr)
 {
@@ -1419,6 +1461,7 @@ static int __init gicv3_of_init(struct device_node *node, struct device_node *pa
 		goto out_unmap_rdist;
 
 	gic_populate_ppi_partitions(node);
+	pm_register_wakeup_device(&gic_wakeup_device);
 
 	if (static_branch_likely(&supports_deactivate_key))
 		gic_of_setup_kvm_info(node);

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -36,6 +36,9 @@
 #include <linux/bitmap.h>
 
 #include <linux/pinctrl/pinctrl_mi.h>
+#include <linux/power_debug.h>
+#include <linux/wakeup_reason.h>
+#include <linux/syscore_ops.h>
 
 #include "../core.h"
 #include "../pinconf.h"
@@ -1585,6 +1588,44 @@ int msm_gpio_mpm_wake_set(unsigned int gpio, bool enable)
 }
 EXPORT_SYMBOL(msm_gpio_mpm_wake_set);
 
+static bool msm_pinctrl_check_wakeup_event(void *data)
+{
+	int i, irq;
+	bool ret = false;
+	u32 val;
+	unsigned long flags;
+	struct irq_desc *desc;
+	const struct msm_pingroup *g;
+	const char *name = "null";
+	struct msm_pinctrl *pctrl= msm_pinctrl_data;
+
+	raw_spin_lock_irqsave(&pctrl->lock, flags);
+	for_each_set_bit(i, pctrl->enabled_irqs, pctrl->chip.ngpio) {
+		g = &pctrl->soc->groups[i];
+		val = readl_relaxed(pctrl->regs + g->intr_status_reg);
+		if (val & BIT(g->intr_status_bit)){
+			irq = irq_find_mapping(pctrl->chip.irq.domain, i);
+			log_irq_wakeup_reason(irq);
+			desc = irq_to_desc(irq);
+			if (desc == NULL)
+				name = "stray_irq";
+			else if (desc->action && desc->action->name)
+				name = desc->action->name;
+			ret = true;
+			pr_warn("%s:%d triggered %s\n", __func__, irq, name);
+		}
+	}
+
+	raw_spin_unlock_irqrestore(&pctrl->lock, flags);
+
+	return ret;
+}
+
+static struct wakeup_device msm_pinctrl_wakeup_device = {
+	.name = "pinctrl-msm",
+	.check_wakeup_event = msm_pinctrl_check_wakeup_event,
+};
+
 int msm_pinctrl_probe(struct platform_device *pdev,
 		      const struct msm_pinctrl_soc_data *soc_data)
 {
@@ -1650,6 +1691,7 @@ int msm_pinctrl_probe(struct platform_device *pdev,
 	platform_set_drvdata(pdev, pctrl);
 
 	register_syscore_ops(&msm_pinctrl_pm_ops);
+	pm_register_wakeup_device(&msm_pinctrl_wakeup_device);
 	dev_dbg(&pdev->dev, "Probed Qualcomm pinctrl driver\n");
 
 	return 0;